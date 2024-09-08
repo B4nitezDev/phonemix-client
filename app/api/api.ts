3f55e1b7bd355901cb6983f1b54115c4f8839ce4
benitez <ebenitez@bizlogit.net>
2024-09-08 00:15:04 -0300

    chore: (API) cambio url del backend principal

--- app/api/api.ts
+++ app/api/api.ts
@@ -1,43 +1,46 @@
 import { ValidationMessage, GetFeedbackResponse } from "../interfaces/response";
-import { TextValidation } from '../interfaces/requests';
+import { TextValidation } from "../interfaces/requests";
+
+const URL_BASE: string = "https://phonemix-api-production.up.railway.app";
 
 export const API = {
-    FEEDBACK: {
-        GET_LANGUAGES: async (): Promise<string[]> => {
-            const response = await fetch("http://ec2-3-143-225-37.us-east-2.compute.amazonaws.com:8000/supported_languages",{
-                method: "GET"
-            });
+  FEEDBACK: {
+    GET_LANGUAGES: async (): Promise<string[]> => {
+      const response = await fetch(URL_BASE + "/supported_languages", {
+        method: "GET",
+      });
 
-            return await response.json();
-        },
-        GET_VALIDATION: async ({text, language}: TextValidation): Promise<ValidationMessage> => {
-            const response = await fetch("http://ec2-3-143-225-37.us-east-2.compute.amazonaws.com:8000/lang_validation", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify({
-                    expected_text: text,
-                    language: language
-                })
-            });
-        
-            return response.json();
-        },
-        GET_FEEDBACK: async (formData: FormData): Promise<GetFeedbackResponse> => {
-            const response: Response = await fetch('http://ec2-3-143-225-37.us-east-2.compute.amazonaws.com:8000/get_feedback', {
-                method: 'POST',
-                headers: {
-                    'accept': 'application/json',
-                    //'Content-Type': 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW'
-                },
-                body: formData
-            });
-
-            return await response.json()
-        }
+      return await response.json();
     },
-    SUPABASE: {
-        
-    }
-}
+    GET_VALIDATION: async ({
+      text,
+      language,
+    }: TextValidation): Promise<ValidationMessage> => {
+      const response = await fetch(URL_BASE + "/lang_validation", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          expected_text: text,
+          language: language,
+        }),
+      });
+
+      return response.json();
+    },
+    GET_FEEDBACK: async (formData: FormData): Promise<GetFeedbackResponse> => {
+      const response: Response = await fetch(URL_BASE + "/get_feedback", {
+        method: "POST",
+        headers: {
+          accept: "application/json",
+          //'Content-Type': 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW'
+        },
+        body: formData,
+      });
+
+      return await response.json();
+    },
+  },
+  SUPABASE: {},
+};
